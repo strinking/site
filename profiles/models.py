77865a63c0d59a6ed60f936ebd8058e7f097fffc
Johannes Christ <Johannes.christ@gmail.com>
2018-05-22 14:03:02 +0200

    Add the `restrict processing` table.

---
+++ profiles/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+
+
+class RestrictProcessing(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    restrict_processing = models.BooleanField(
+        default=False,
+        help_text="Make your profile invisible to other users."
+    )
